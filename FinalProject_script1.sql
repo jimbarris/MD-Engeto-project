d051d7cb88de162722a962934f05bf4a4f3cd5d1
jimbarris¨ <email@marekduda.cz>
2025-08-28 16:50:38 +0200

    edit scripts

--- FinalProject_script1.sql
+++ FinalProject_script1.sql
@@ -12,7 +12,7 @@ SELECT
 	,cpc.name AS food_category
 	,cpc.price_value
 	,cpc.price_unit
-	,cpib.name AS industry_branch
+	,cpib.name AS industry_branch_name
 FROM czechia_price AS cp
 	JOIN czechia_payroll AS cpay 
 		ON YEAR(cp.date_from) = cpay.payroll_year
@@ -25,4 +25,4 @@ FROM czechia_price AS cp
 
 -- Select do nově vytvořené tabulky FINAL1 pro kontrolu.
 
-SELECT * FROM t_marek_duda_project_SQL_primary_final
+SELECT * FROM t_marek_duda_project_SQL_primary_final;
--- FinalProject_script2.sql
+++ FinalProject_script2.sql
@@ -1,7 +1,7 @@
 -- TABLE 2 - Dodatečná data o dalších evropských státech (období 2006–2018) 
 
 
-CREATE TABLE t_marek_duda_project_SQL_secondary_final AS 
+CREATE OR REPLACE TABLE t_marek_duda_project_SQL_secondary_final AS 
 SELECT 
 	c.country
 	,e.year
